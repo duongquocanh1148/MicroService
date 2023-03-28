76f553338708336936b2616cb78ee957eef45120
Duong Quoc Anh <anhdq1@imt-soft.com>
2023-03-28 11:20:03 +0700

    Done IAM Service

---
+++ IAMService/src/main/java/com/example/demo/controller/AuthenticationController.java
@@ -0,0 +1,60 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.*;
+import com.example.demo.service.AuthenticateService;
+import com.example.demo.service.RegisterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.mail.MessagingException;
+import java.io.UnsupportedEncodingException;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthenticationController {
+    @Autowired
+    private final AuthenticateService authenticateService;
+
+
+    @Autowired
+    private RegisterService registerService;
+
+    public AuthenticationController(AuthenticateService authenticateService) {
+        this.authenticateService = authenticateService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<ResponseObject> register(
+            @RequestBody Register request
+    ) {
+        return ResponseEntity.ok(registerService.register(request));
+    }
+
+    @PostMapping("/confirm")
+    public ResponseEntity<ResponseObject> confirmEmail(
+            @RequestBody ConfirmRequest confirmRequest
+    ) {
+        return ResponseEntity.ok(registerService.confirmEmail(confirmRequest));
+    }
+
+    @PostMapping("/sendToken")
+    public ResponseEntity sendToken(
+            @RequestBody EmailRequest request
+    ) throws MessagingException, UnsupportedEncodingException {
+        return ResponseEntity.ok(registerService.sendToken(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ResponseObject> authenticate(
+            @RequestBody AuthenticateRequest request
+    ) {
+        return ResponseEntity.ok(authenticateService.authenticate(request));
+    }
+
+
+
+}
---
+++ IAMService/src/main/java/com/example/demo/model/Token.java
@@ -0,0 +1,28 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Token {
+    @Id
+    @GeneratedValue
+    public Integer id;
+    @Column(unique = true)
+    public String token;
+    public String tokenType = "BEARER";
+    public boolean revoked;
+    public boolean expired;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    public Users user;
+
+}
