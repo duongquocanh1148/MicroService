6e4e42091afb47e8aeeb52ca74989b0cc0c0e5ce
Duong Quoc Anh <anhdq1@imt-soft.com>
2023-03-30 10:58:54 +0700

    Add Swagger 2

--- IAMService/src/main/java/com/example/demo/config/SecurityConfig.java
+++ IAMService/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.demo.config;
 
-import com.example.demo.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
@@ -9,20 +8,13 @@ import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurer
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
@@ -32,48 +24,16 @@ import org.springframework.security.web.authentication.session.SessionAuthentica
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
-    /**
-     * Registers the KeycloakAuthenticationProvider with the authentication manager.
-     */
-    private final AuthenticationProvider authenticationProvider;
-//    @Autowired
-//    private final UserRepository userRepository;
-//    @Bean
-//    public UserDetailsService userDetailsService(){
-//
-//        return username -> userRepository.findByUserName(username)
-//                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-//    }
-//    @Bean
-//    public AuthenticationProvider authenticationProvider(){
-//        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-//        authProvider.setUserDetailsService(userDetailsService());
-//        authProvider.setPasswordEncoder(passwordEncoder());
-//        return authProvider;
-//    }
-//    @Bean
-//    public PasswordEncoder passwordEncoder()
-//    {
-//        return new BCryptPasswordEncoder();
-//    }
-//    @Bean
-//    public AuthenticationManager authenticationManagerBean() throws Exception {
-//        return super.authenticationManagerBean();
-//    }
-    @Autowired
 
+    private final AuthenticationProvider authenticationProvider;
+
+    @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         KeycloakAuthenticationProvider authenticationProvider1 = new KeycloakAuthenticationProvider();
         authenticationProvider1.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
-        auth.authenticationProvider(authenticationProvider1)
-        .authenticationProvider(authenticationProvider);
-
-
+        auth.authenticationProvider(authenticationProvider1).authenticationProvider(authenticationProvider);
     }
 
-    /**
-     * Defines the session authentication strategy.
-     */
     @Bean
     @Override
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
@@ -83,12 +43,7 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
-        http
-                .csrf()
-                .disable()
-                .authorizeRequests()
-
-                .anyRequest().permitAll();
+        http.csrf().disable().authorizeRequests().anyRequest().permitAll();
 
     }
 
